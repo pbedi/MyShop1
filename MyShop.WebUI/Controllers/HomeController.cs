bd1e657a2afd7f9394775931915044189fba4846
Philip <philip.bedi@berkeleygroup.co.uk>
2018-12-05 18:57:29 +0000

    10.Product Listing Home controller

--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,32 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            this.context = productContext;
+            this.productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List < Product > products = context.Collection().ToList();
+            return View(products);
         }
 
-        public ActionResult About()
+        public ActionResult Details(string Id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            Product product = context.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            } else
+            {
+                return View(product);
+            }
+            
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
