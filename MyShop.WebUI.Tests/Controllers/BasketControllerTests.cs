3f690b0a1a4d2ea2af4108ff4139b88e1fed31e2
Philip <philip.bedi@berkeleygroup.co.uk>
2018-12-21 18:05:30 +0000

    Unite test - Basket Summary

--- MyShop.WebUI.Tests/Controllers/BasketControllerTests.cs
+++ MyShop.WebUI.Tests/Controllers/BasketControllerTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.Services;
 using MyShop.WebUI.Controllers;
 using MyShop.WebUI.Tests.Mocks;
@@ -58,5 +60,40 @@ namespace MyShop.WebUI.Tests.Controllers
             Assert.AreEqual(1, basket.BasketItems.Count);
             Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
         }
+
+        [TestMethod]
+        public void CanGetSummaryViewModel()
+        {
+            //setup
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+            products.Insert(new Product() { Id = "1", Price = 10.00m });
+            products.Insert(new Product() { Id = "2", Price = 5.00m });
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem() { ProductId = "1", Quantity = 2 });
+            basket.BasketItems.Add(new BasketItem() { ProductId = "2", Quantity = 1 });
+
+            baskets.Insert(basket);
+
+            IBasketService basketService = new BasketService(products, baskets);
+
+            var httpContext = new MockHttpContext();
+
+            var controller = new BasketController(basketService);
+            httpContext.Request.Cookies.Add(new System.Web.HttpCookie("eCommerceBasket") { Value = basket.Id });
+            controller.ControllerContext = new System.Web.Mvc.ControllerContext(httpContext, new System.Web.Routing.RouteData(), controller);
+
+            var result = controller.BasketSummary() as PartialViewResult;
+            var basketSummary = (BasketSummaryViewModel)result.ViewData.Model;
+
+            //Assert
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(2, basket.BasketItems.Count);
+            Assert.AreEqual(3, basketSummary.BasketCount);
+            Assert.AreEqual(25.00m, basketSummary.BasketTotal);
+        }
+
     }
 }
